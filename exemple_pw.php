5ad1a0edd6b31483eaa3cb7c80ac559cfa7404a8
root <root@aneva-pc-hp.(none)>
2014-06-18 11:21:56 +0200

    A voir

---
+++ exemple_pw.php
@@ -0,0 +1,11 @@
+<html>
+<?php
+include ("miseenpage.php");
+
+/*autentification d'un client */
+
+echo "</br><a href=\"http://lcalhost/fichier1.php\"><button type=\"button\">Document 1 </button></a>";
+echo "</br><a href=\"http://localhost/fichier2.php\"><button type=\"button\">Document 2</button></a>";
+
+?>
+</html>
